ac8eb901875bdeec79a272847a46bae82f7240f9
Paul Zabelin <paulz@users.noreply.github.com>
2021-06-10 00:46:26 -0700

    measure performance including image loading

--- Example/Tests/PerformanceTest.swift
+++ Example/Tests/PerformanceTest.swift
@@ -5,7 +5,12 @@ import XCTest
  Compare performance of image comparison
  using CoreImage vs vImage frameworks
 
- on iMacPro performance of CoreImage is 4 times faster (0.0015753 vs 0.0060786)
+ on iMacPro
+ for preloaded images
+ performance of CoreImage is 4 times faster (0.0015753 vs 0.0060786)
+
+ including image load
+ performance of CoreImage is 1.5 times slower (0.018587 vs 0.011776)
  */
 class PerformanceTest: XCTestCase {
     let bundle = Bundle(for: PerformanceTest.self)
@@ -15,6 +20,16 @@ class PerformanceTest: XCTestCase {
     override func setUpWithError() throws {
         image1Url = bundle.urlForImageResource("AvatarView-1.png")!
         image2Url = bundle.urlForImageResource("AvatarView-2.png")!
+    }
+
+    func test_CoreImage_AndLoad_Performance() throws {
+        measure {
+            let image1 = CIImage(contentsOf: image1Url)!
+            let image2 = CIImage(contentsOf: image2Url)!
+            let diffOperation = diff(image1, image2)
+            let diffOutput = diffOperation.outputImage!
+            _ = maxColorDiff(histogram: histogram(ciImage: diffOutput))
+        }
     }
 
     func test_CoreImage_Performance() throws {
@@ -32,8 +47,6 @@ class PerformanceTest: XCTestCase {
 
     func test_vImage_Buffer_Performance() throws {
         let data1 = try! Data(contentsOf: image1Url)
-
-        let image2Url = bundle.urlForImageResource("AvatarView-2.png")!
         let data2 = try! Data(contentsOf: image2Url)
         let cgImage2 = NSBitmapImageRep(data: data2)!.cgImage!
 
@@ -45,4 +58,14 @@ class PerformanceTest: XCTestCase {
             _ = significantlyDifferentImages(data1, cgImage2)
         }
     }
+
+    func test_vImage_Buffer_AndLoad_Performance() throws {
+        measure {
+            let data1 = try! Data(contentsOf: image1Url)
+            let data2 = try! Data(contentsOf: image2Url)
+            let cgImage2 = NSBitmapImageRep(data: data2)!.cgImage!
+            _ = significantlyDifferentImages(data1, cgImage2)
+        }
+    }
+
 }
