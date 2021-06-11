e9c7c0a4288118429581782bf2d98b6991ab819a
Paul Zabelin <paulz@users.noreply.github.com>
2021-06-10 23:36:08 -0700

    test: snapshot mismatch

--- Example/Tests/MatchingSnapshotFailureSpec.swift
+++ Example/Tests/MatchingSnapshotFailureSpec.swift
@@ -6,13 +6,20 @@ import SwiftUI
 class MatchingSnapshotFailureSpec: QuickSpec {
     override func spec() {
         describe("MatchingSnapshot") {
+            var fileManager: FileManager!
+            var snapshotUrl: URL!
+            var tempFolder: URL!
+            beforeEach {
+                fileManager = FileManager.default
+                tempFolder = URL(fileURLWithPath: "/tmp/")
+                MatchingSnapshot.configuration.snapshotsFolderUrl = tempFolder
+            }
+            afterEach {
+                MatchingSnapshot.configuration.snapshotsFolderUrl = nil
+                precondition(fileManager.fileExists(atPath: snapshotUrl.path))
+            }
             context("no snapshot") {
-                var fileManager: FileManager!
-                var snapshotUrl: URL!
                 beforeEach {
-                    let tempFolder = URL(fileURLWithPath: "/tmp/")
-                    MatchingSnapshot.configuration.snapshotsFolderUrl = tempFolder
-                    fileManager = FileManager.default
                     snapshotUrl = tempFolder.appendingPathComponent("EmptyView.png")
                     try? fileManager.removeItem(at: snapshotUrl)
                     precondition(!fileManager.fileExists(atPath: snapshotUrl.path))
@@ -22,14 +29,36 @@ class MatchingSnapshotFailureSpec: QuickSpec {
                             && issue.compactDescription ==
                             "EmptyView.png was missing, now recorded\n"
                     }
-                    XCTExpectFailure("should fail and record", options: options)
-                }
-                afterEach {
-                    MatchingSnapshot.configuration.snapshotsFolderUrl = nil
-                    precondition(fileManager.fileExists(atPath: snapshotUrl.path))
+                    XCTExpectFailure("and record", options: options)
                 }
                 itBehavesLike(MatchingSnapshot.self) {
                     SwiftUIView(EmptyView())
+                }
+            }
+            context("mismatch") {
+                var differentImageUrl: URL!
+                beforeEach {
+                    snapshotUrl = tempFolder.appendingPathComponent("hello.png")
+                    let bundle = Bundle(for: Self.self)
+                    differentImageUrl = bundle.urlForImageResource("AvatarView-1.png")!
+                    try? fileManager.removeItem(at: snapshotUrl)
+                    try! fileManager.copyItem(at: differentImageUrl, to: snapshotUrl)
+                    let options = XCTExpectedFailure.Options()
+                    options.issueMatcher = { issue in
+                        issue.type == .assertionFailure
+                            && issue.compactDescription ==
+                            "hello.png was different, now recorded\n"
+                    }
+                    XCTExpectFailure("and overwrite", options: options)
+
+                }
+                itBehavesLike(MatchingSnapshot.self) {
+                    SwiftUIView(Text("hello").background(Color.black), name: "hello")
+                }
+                afterEach {
+                    let expected = try! Data(contentsOf: differentImageUrl)
+                    let actual = try! Data(contentsOf: snapshotUrl)
+                    precondition(expected != actual)
                 }
             }
         }
